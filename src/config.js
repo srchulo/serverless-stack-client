b82d4c20f7f13b14645342740bd5d9a136f595c2
Adam Hopkins <violapiratejunky@gmail.com>
2020-03-29 11:56:10 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-srchulo"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://g5sfb4e8hb.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_sXesnt3Dl",
+      APP_CLIENT_ID: "1lkuf9bm5dpf7q75qiiplnmeqq",
+      IDENTITY_POOL_ID: "us-east-2:58283dde-a75d-432a-97e6-59995e8688fa"
+    }
+  };
